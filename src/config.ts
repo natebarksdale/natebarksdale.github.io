6b9fb533196a4d08454bca3e04133999cd33da8d
Nate Barksdale <nbarksdale@eventidefunds.com>
2025-02-11 16:11:56 +0000

    ibid

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "Nate Barksdale Writing + Design",
   ogImage: "natebarksdale-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 50,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
