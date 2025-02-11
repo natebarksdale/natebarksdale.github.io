9b21d331f669d978c3ad170299f8cdbf040e8fc3
Nate Barksdale <nbarksdale@eventidefunds.com>
2025-02-11 16:29:58 +0000

    ibid

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ export interface Props {
 }
 
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
-  const { title, pubDatetime, modDatetime, description } = frontmatter;
+  const { title, pubDatetime, modDatetime, description, emoji, haiku } = frontmatter;
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
