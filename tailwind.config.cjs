8c448c8bea5a1eb1dd4a5e0acfa20c0785872b04
natebarksdale <natebarksdale@gmail.com>
2025-02-21 12:57:21 -0500

    ibid

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -80,7 +80,32 @@ module.exports = {
     },
   },
   plugins: [
-    require("@tailwindcss/typography"),
+    require("@tailwindcss/typography")({
+      className: 'prose',
+      // Customize the prose plugin
+      target: 'modern',
+      overrides: {
+        css: {
+          '--tw-prose-links': 'rgb(var(--color-accent))',
+          'a': {
+            'text-decoration': 'none',
+            'background': 'linear-gradient(0deg, rgb(var(--color-accent) / 0.5) 0%, rgb(var(--color-accent) / 0.5) 100%)',
+            'background-position': '0 85%',
+            'background-repeat': 'no-repeat',
+            'background-size': '100% 0.4em',
+            'text-shadow': [
+              '0.1em 0 var(--color-fill)',
+              '-0.1em 0 var(--color-fill)',
+              '0 0.1em var(--color-fill)',
+              '0 -0.1em var(--color-fill)'
+            ].join(','),
+            '&:hover': {
+              color: 'rgb(var(--color-accent))',
+            },
+          },
+        },
+      },
+    }),
     function({ addUtilities, theme }) {
       addUtilities({
         '.fancy-underline': {
