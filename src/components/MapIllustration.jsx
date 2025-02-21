2532f1b75b0186a1a3673f40d9d7e8b7aa2e82a5
natebarksdale <natebarksdale@gmail.com>
2025-02-21 14:26:09 -0500

    Adjust MapIllustration component margins and letter positioning

--- src/components/MapIllustration.jsx
+++ src/components/MapIllustration.jsx
@@ -74,7 +74,7 @@ const MapIllustration = ({
         style={{
           y: mapParallax,
           clipPath: generateClipPath(),
-          marginTop: "4rem",
+          marginTop: "2rem",
           zIndex: 1,
         }}
       >
@@ -97,7 +97,7 @@ const MapIllustration = ({
             y: letterParallax[index],
             zIndex: index + 2,
             clipPath: pos.clipPath,
-            marginTop: `${20 + Math.random() * 5}rem`,
+            marginTop: `${2 + Math.random() * 10}rem`,
           }}
         >
           <motion.svg
