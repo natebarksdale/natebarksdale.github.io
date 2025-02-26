6c09ec8fe3f7e8bce69491c1c934dfc5a47df54d
natebarksdale <natebarksdale@gmail.com>
2025-02-26 15:01:07 -0500

    ibid

--- src/components/MapIllustration.jsx
+++ src/components/MapIllustration.jsx
@@ -96,8 +96,8 @@ const MapIllustration = ({
       if (pathType === "straight") {
         lines.push(`M0,${i * lineSpacing} L${width},${i * lineSpacing}`);
       } else {
-        const controlPoint1 = (Math.random() * width) / 4;
-        const controlPoint2 = (Math.random() * width) / 4 + width / 2;
+        const controlPoint1 = (Math.random() * width) / 8; // Tighter curve
+        const controlPoint2 = (Math.random() * width) / 8 + width / 2;
         lines.push(
           `M0,${i * lineSpacing} Q${controlPoint1},${i * lineSpacing + lineSpacing / 2} ${width / 2},${i * lineSpacing} T${width},${i * lineSpacing}`
         );
