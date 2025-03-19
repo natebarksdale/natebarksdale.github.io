48077404092f9c65d7429ec0e84e63cf04b8e37a
natebarksdale <natebarksdale@gmail.com>
2025-03-19 17:14:20 -0400

    ibid

--- src/components/ShareModal.tsx
+++ src/components/ShareModal.tsx
@@ -120,14 +120,6 @@ const ShareModal: React.FC<ShareModalProps> = ({ url, title, description }) => {
         className="flex items-center gap-1 hover:opacity-75"
         onClick={() => setIsOpen(true)}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon-tabler">
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-          <circle cx="6" cy="12" r="3"></circle>
-          <circle cx="18" cy="6" r="3"></circle>
-          <circle cx="18" cy="18" r="3"></circle>
-          <line x1="8.7" y1="10.7" x2="15.3" y2="7.3"></line>
-          <line x1="8.7" y1="13.3" x2="15.3" y2="16.7"></line>
-        </svg>
         Share this Post
       </button>
 
